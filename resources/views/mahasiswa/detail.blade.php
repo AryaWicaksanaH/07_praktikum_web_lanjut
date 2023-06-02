751b39117b8804c87e18f05a458177c0ec5f2aec
= <=>
2023-06-02 08:48:17 +0700

    Latihan Praktikum 9: Membuat relasi many to many

--- resources/views/mahasiswa/detail.blade.php
+++ resources/views/mahasiswa/detail.blade.php
@@ -1,25 +1,22 @@
-@extends('layouts.app')
+@extends('mahasiswas.layout') @section('content')
+<div class="container mt-5">
+<div class="row justify-content-center align-items-center">
+<div class="card" style="width: 24rem;">
+<div class="card-header"> Detail Mahasiswa
+</div>
+<div class="card-body">
+<ul class="list-group list-group-flush">
+<li class="list-group-item"><b>Nim: </b>{{$Mahasiswa->Nim}}</li>
+<li class="list-group-item"><b>Nama: </b>{{$Mahasiswa->Nama}}</li>
+<li class="list-group-item"><b>Tetala: </b>{{$Mahasiswa->Tetala}}</li>
+<li class="list-group-item"><b>Kelas: </b>{{$Mahasiswa->Kelas->nama_kelas}}</li>
+<li class="list-group-item"><b>Jurusan: </b>{{$Mahasiswa->Jurusan}}</li>
+<li class="list-group-item"><b>E-Mail: </b>{{$Mahasiswa->Email}}</li>
+<li class="list-group-item"><b>No_Handphone: </b>{{$Mahasiswa->No_Handphone}}</li>
+</ul>
+</div>
+<a class="btn btn-success mt-3" href="{{ route('mahasiswas.index') }}">Kembali</a>
 
-@section('content')
-    <div class="container mt-5">
-        <div class="d-flex justify-content-center align-items-center">
-            <div class="card" style="width: 24rem;">
-                <div class="card-header">
-                    Detail Mahasiswa
-                </div>
-                <div class="card-body">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><b>Nim: </b>{{ $mahasiswa->nim }}</li>
-                        <li class="list-group-item"><b>Nama: </b>{{ $mahasiswa->nama }}</li>
-                        <li class="list-group-item"><b>Kelas: </b>{{ $mahasiswa->kelas->nama_kelas}}</li>
-                        <li class="list-group-item"><b>Jurusan: </b>{{ $mahasiswa->jurusan }}</li>
-                        <li class="list-group-item"><b>No Handphone: </b>{{ $mahasiswa->no_hp }}</li>
-                        <li class="list-group-item"><b>Email: </b>{{ $mahasiswa->email }}</li>
-                        <li class="list-group-item"><b>Tanggal Lahir: </b>{{ $mahasiswa->tgl_lahir }}</li>
-                    </ul>
-                </div>
-                <a class="btn btn-success mt-3" href="{{ route('mahasiswa.index') }}">Kembali</a>
-            </div>
-        </div>
-    </div>
-@endsection
+</div>
+</div>
+</div> @endsection
